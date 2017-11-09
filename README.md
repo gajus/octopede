a9b8fcc8cd2b85d0a7243125f20ec3a69e2b1470
Gajus Kuizinas <gajus@gajus.com>
2017-11-09 09:56:44 +0000

    feat: release headless Chrome orchestration tool

---
+++ README.md
@@ -0,0 +1,19 @@
+# Octopedes
+
+[![Travis build status](http://img.shields.io/travis/gajus/octopedes/master.svg?style=flat-square)](https://travis-ci.org/gajus/octopedes)
+[![Coveralls](https://img.shields.io/coveralls/gajus/octopedes.svg?style=flat-square)](https://coveralls.io/github/gajus/octopedes)
+[![NPM version](http://img.shields.io/npm/v/octopedes.svg?style=flat-square)](https://www.npmjs.org/package/octopedes)
+[![Canonical Code Style](https://img.shields.io/badge/code%20style-canonical-blue.svg?style=flat-square)](https://github.com/gajus/canonical)
+[![Twitter Follow](https://img.shields.io/twitter/follow/kuizinas.svg?style=social&label=Follow)](https://twitter.com/kuizinas)
+
+Headless Chrome orchestration service.
+
+## Usage
+
+```bash
+$ octopedes start \
+  --maximum-instances 10 \
+  --minimum-instances 5 \
+  --port 9200
+
+```
---
+++ src/bin/commands/start.js
@@ -0,0 +1,127 @@
+// @flow
+
+import puppeteer from 'puppeteer';
+import {
+  createPool
+} from 'generic-pool';
+import WebSocket from 'ws';
+import Logger from '../../Logger';
+
+export const command = 'start';
+export const desc = 'Starts a cluster of headless Chrome instances.';
+
+const log = Logger.child({
+  command
+});
+
+// eslint-disable-next-line flowtype/no-weak-types
+export const builder = (yargs: Object) => {
+  return yargs
+    .options({
+      'maximum-instances': {
+        default: 1,
+        describe: 'Maximum number of Chrome instances open at any time.',
+        type: 'number'
+      },
+      'minimum-instances': {
+        default: 0,
+        describe: 'Minimum number of Chrome instances open at any time.',
+        type: 'number'
+      },
+      port: {
+        default: 9000,
+        type: 'number'
+      }
+    });
+};
+
+type ArgvType = {|
+  +maximumInstances: number,
+  +minimumInstances: number,
+  +port: number
+|};
+
+export const handler = async (argv: ArgvType) => {
+  log.debug('starting a headless Chrome instance cluster under port %d', argv.port);
+
+  const pool = createPool({
+    create: async () => {
+      const browser = await puppeteer.launch();
+
+      const url = browser._connection.url();
+
+      log.debug('created Chrome (url: %s)', url);
+
+      return {
+        browser,
+        url
+      };
+    },
+    destroy: async (instance) => {
+      await instance.browser.close();
+
+      log.debug('destroyed Chrome (url: %s)', instance.url);
+    }
+  }, {
+    max: argv.maximumInstances,
+    min: argv.minimumInstances
+  });
+
+  pool.start();
+
+  const wss = new WebSocket.Server({
+    port: argv.port
+  });
+
+  wss.on('connection', (ws) => {
+    const messageQueue = [];
+
+    let server;
+
+    pool
+      .acquire()
+      // eslint-disable-next-line promise/always-return
+      .then((instance) => {
+        const wsc = new WebSocket(instance.url);
+
+        wsc.on('open', () => {
+          log.debug('connected to Chrome (url: %s)', instance.url);
+
+          server = wsc;
+
+          for (const message of messageQueue) {
+            wsc.send(message);
+          }
+        });
+
+        wsc.on('close', () => {
+          log.debug('Chrome disconnected (url: %s)', instance.url);
+
+          server = null;
+        });
+
+        wsc.on('message', (message) => {
+          if (ws.readyState === WebSocket.OPEN) {
+            ws.send(message);
+          }
+        });
+
+        ws.on('close', () => {
+          log.debug('client disconnected');
+
+          pool.destroy(instance);
+        });
+      })
+      .catch(() => {
+        log.fatal('failed to acquire Chrome instance');
+      });
+
+    ws.on('message', (message) => {
+      if (server) {
+        server.send(message);
+      } else {
+        messageQueue.push(message);
+      }
+    });
+  });
+};
